793f9b4d824b46a22acd716a436553633b5dc514
milka <mileto94@abv.bg>
2015-06-08 16:07:31 +0300

    Add option to create playlist

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 Using this fancy app you can listen music songs, edit their metadata, get more ingo about them. Only with one click you can create playlists, adding all of your favourite songs to one place.
 
-*Behind the scenes this app gets name of file and checks if it exists. If yes, than it gets
- its metadata such as artist, length, format(mp3, mp4, wav and etc) and alows user to change some of them. Then the app gets abspath of converted file and saves it to **sqllite3** db called music_player
+> Behind the scenes this app gets name of file and checks if it exists. If yes, than it gets
+ its metadata such as artist, length, format(mp3, mp4, wav and etc) and alows user to change some of them. Then the app gets abspath of converted file and saves it to* **sqllite3** *db called music_player
 . When starting the player for the first time, it creates new hidden folder in its folder where it saves converted songs to .wav files via pydub lib.
 When user wants to play song, the song will be played from the hidden folder.
 
--- factory.py
+++ factory.py
@@ -1,4 +1,4 @@
-from create_database import Song, create_db
+from create_database import Song, Playlist, create_db
 from os.path import abspath, exists
 from pydub import AudioSegment
 from sqlalchemy.orm import Session
@@ -19,31 +19,60 @@ def insert_song_into_db(engine, song_info):
     )
     session.add(song)
     session.commit()
+    return song
 
 
-def create_song(db_name, path, name=''):
+def insert_playlist_into_db(engine, playlist_info):
+    session = Session(bind=engine)
+
+    if session.query(Playlist).filter(
+            Playlist.name == playlist_info['name']).all():
+        playlist = session.query(Playlist).filter(
+            Playlist.name == playlist_info['name']).all()
+    else:
+        playlist = Playlist(
+            name=playlist_info['name'],
+            song_id=playlist_info['song_id'],
+
+        )
+        session.add(playlist)
+        session.commit()
+    return playlist
+
+
+def create_song(db_name, path, name='', playlist_name='unknown'):
     folder, engine = create_db(db_name)
 
     if exists(path):
         old_song = AudioSegment.from_mp3(path)
         song_name = '{}.wav'.format(path.split('.')[0])
-        converted_song = old_song.export(song_name, format='wav')
+        # converted_song = old_song.export(song_name, format='wav')
 
         original_song = EasyID3(path)
-        song = {}
-        song['name'] = original_song['title'][0]
-        song['path'] = abspath('{}.wav'.format(song_name))
-        song['artist'] = original_song['artist'][0]
+
+        song = {
+            'name': original_song['title'][0],
+            'path': abspath(song_name),
+            'artist': original_song['artist'][0],
+            'length': old_song.duration_seconds,
+            'album': original_song['album'][0]
+        }
+        # original_song = MP3(path)
         # song['length'] = original_song.info.length
-        song['length'] = old_song.duration_seconds
-        song['album'] = original_song['album'][0]
-        insert_song_into_db(engine, song)
+
+        created_song = insert_song_into_db(engine, song)
+        playlist_info = {
+            'name': playlist_name,
+            'song_id': created_song.id
+        }
+
+        insert_playlist_into_db(engine, playlist_info)
     else:
         raise Exception('There is not such file or directory! Try another one')
 
 
 def main():
-    create_song('music_player', 'song.mp3')
+    create_song('music_player', 'song.mp3', playlist_name='my_playlist')
     # create_song('music_player.db', 'dsf')
 
 
